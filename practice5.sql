e3730fac1df41e90610fbba357657710107c73fb
RobTecGda <78834009+RobTecGda@users.noreply.github.com>
2022-04-25 09:20:52 -0500

    Update practice5.sql

--- practice5.sql
+++ practice5.sql
@@ -7,13 +7,13 @@
 -- Foreign key must exist in the original table
 
 -- Check constraints to put a condition
-CONSTRAINT check_salary_not_below_zero CHECK (salary>=0)
+-- CONSTRAINT check_salary_not_below_zero CHECK (salary>=0)
 
-UNIQUE
-value in the column must be unique
+-- UNIQUE
+-- value in the column must be unique
 
-NOT NULL
-The value must exist and to add this constraint we do directly when creating the column
+-- NOT NULL
+-- The value must exist and to add this constraint we do directly when creating the column
 
 -- Listing 8-1: Declaring a single-column natural key as primary key
 
