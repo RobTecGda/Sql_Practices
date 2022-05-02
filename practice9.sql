d3152a01fe1c0d7d785cdaadcd0adfaf00e3707e
RobTecGda <78834009+RobTecGda@users.noreply.github.com>
2022-05-02 08:58:53 -0500

    Update practice9.sql

--- practice9.sql
+++ practice9.sql
@@ -5,6 +5,8 @@
 -- In a Join
 -- As part of the main select as a calculated column
 -- We use subqueries in an where exists (where in ())
+from outer_table
+where exists (select id, from inner_table where inner_table.id = outer_table.id)
 -- for lateral subqueries
 -- Common Table Expressions, is a temporal table created from a subquery.
 
