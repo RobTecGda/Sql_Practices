0dfa9c23ee797cb7da608b8d1941fc973b3af75a
RobTecGda <78834009+RobTecGda@users.noreply.github.com>
2022-05-02 08:56:10 -0500

    Create practice9.sql

---
+++ practice9.sql
@@ -0,0 +1,13 @@
+-- Subquery
+
+-- Use subqueries in the where condition
+-- Use subqueries in a from
+-- In a Join
+-- As part of the main select as a calculated column
+-- We use subqueries in an where exists (where in ())
+-- for lateral subqueries
+-- Common Table Expressions, is a temporal table created from a subquery.
+
+WITH temporal_table_name (c1,c2,c3 ...)
+as (subquery)
+select
