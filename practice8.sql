194dd8ca5dd01d175a740fd4532bbfe8e343b478
RobTecGda <78834009+RobTecGda@users.noreply.github.com>
2022-05-02 08:14:30 -0500

    Create practice8.sql

---
+++ practice8.sql
@@ -0,0 +1,79 @@
+-- Correspond to the Chapter 12 Date and Time
+
+-- Date: Full date
+-- Time: Time
+-- Interval
+
+-- Operations:
+
+-- Challenge:
+-- Calculate the length of each ride. Sort the query from the longest to the shortest.
+
+
+-- Listing 12-1: Extracting components of a timestamp value using date_part()
+
+SELECT
+    date_part('year', '2022-12-01 18:37:12 EST'::timestamptz) AS year,
+    date_part('month', '2022-12-01 18:37:12 EST'::timestamptz) AS month,
+    date_part('day', '2022-12-01 18:37:12 EST'::timestamptz) AS day,
+    date_part('hour', '2022-12-01 18:37:12 EST'::timestamptz) AS hour,
+    date_part('minute', '2022-12-01 18:37:12 EST'::timestamptz) AS minute,
+    date_part('seconds', '2022-12-01 18:37:12 EST'::timestamptz) AS seconds,
+    date_part('timezone_hour', '2022-12-01 18:37:12 EST'::timestamptz) AS tz,
+    date_part('week', '2022-12-01 18:37:12 EST'::timestamptz) AS week,
+    date_part('quarter', '2022-12-01 18:37:12 EST'::timestamptz) AS quarter,
+    date_part('epoch', '2022-12-01 18:37:12 EST'::timestamptz) AS epoch;
+
+-- Bonus: Using the SQL-standard extract() for similar datetime parsing:
+
+SELECT extract(year from '2022-12-01 18:37:12 EST'::timestamptz) AS year;
+
+-- Listing 12-2: Three functions for making datetimes from components
+
+-- make a date
+SELECT make_date(2022, 2, 22);
+-- make a time
+SELECT make_time(18, 4, 30.3);
+-- make a timestamp with time zone
+SELECT make_timestamptz(2022, 2, 22, 18, 4, 30.3, 'Europe/Lisbon');
+
+-- Bonus: Retrieving the current date and time
+
+SELECT
+    current_timestamp,
+    localtimestamp,
+    current_date,
+    current_time,
+    localtime,
+    now();
+
+-- Listing 12-3: Comparing current_timestamp and clock_timestamp() during row insert
+
+CREATE TABLE current_time_example (
+    time_id integer GENERATED ALWAYS AS IDENTITY,
+    current_timestamp_col timestamptz,
+    clock_timestamp_col timestamptz
+);
+
+INSERT INTO current_time_example
+            (current_timestamp_col, clock_timestamp_col)
+    (SELECT current_timestamp,
+            clock_timestamp()
+     FROM generate_series(1,1000));
+
+SELECT * FROM current_time_example;
+
+-- Time Zones
+
+-- Listing 12-4: Viewing your current time zone setting
+
+SHOW timezone; -- Note: You can see all run-time defaults with SHOW ALL;
+SELECT current_setting('timezone');
+
+-- Using current_setting() inside another function:
+SELECT make_timestamptz(2022, 2, 22, 18, 4, 30.3, current_setting('timezone'));
+
+-- Listing 12-5: Showing time zone abbreviations and names
+
+SELECT * FROM pg_timezone_abbrevs ORDER BY abbrev;
+SELECT * FROM pg_timezone_names ORDER BY name;
