0618568c0dfe9fc61abc489d1c6c9b3af4f1a910
RobTecGda <78834009+RobTecGda@users.noreply.github.com>
2022-05-03 08:50:13 -0500

    Create practice11.sql

---
+++ practice11.sql
@@ -0,0 +1,11 @@
+-- Mining Text
+  -- Plain Text
+  -- Regular expression
+  
+--> strings
+--> regex
+--> text search
+
+for practice regular expressions
+https://regex101.com/
+https://en.wikipedia.org/wiki/Regular_expression
